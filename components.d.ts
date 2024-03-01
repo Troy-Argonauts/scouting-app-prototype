2622fa803d82703a6f47e96e04a0c6741a8b559f
John Sanders <jws121295@gmail.com>
2024-02-29 22:14:05 -0500

    build: yarn what the heck

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ContentCopy: typeof import('./src/components/ContentCopy.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     QRCodeScanner: typeof import('./src/components/QRCodeScanner.vue')['default']
     ScoutingForm: typeof import('./src/components/ScoutingForm.vue')['default']
   }
