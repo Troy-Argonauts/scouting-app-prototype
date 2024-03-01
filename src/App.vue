6b895e9eaa9845e869e1ae1bf32803ecab7180f0
John Sanders <jsanders@redviking.com>
2024-02-29 19:50:33 -0500

    feat: tab modes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <v-app>
-    <v-main>
-        <ScoutingForm />
-        <v-textarea
-            v-model="content"
-            style="width:100%"
-        />
-        <QRCodeScanner
-            @result="onQrScan"
-        />
-        <qr-code
-            :contents="debouncedContent"
-        />
+    <v-main style="background-color: #d8d8d8">
+        <v-container>
+            <v-row>
+                <v-col style="max-width: 6in" class="mx-auto">
+
+                    <v-card>
+                        <v-tabs v-model="mode">
+                            <v-tab value="entry">Entry</v-tab>
+                            <v-tab value="scan">Scan</v-tab>
+                        </v-tabs>
+            
+                        <v-card-text>
+                            <v-window v-model="mode">
+                                <v-window-item value="entry">
+                                    <ScoutingForm />
+                                    <v-textarea
+                                        v-model="content"
+                                        style="width:100%"
+                                    />
+                                    <qr-code
+                                        :contents="debouncedContent"
+                                    />
+                                </v-window-item>
+                                <v-window-item value="scan">
+                                    <QRCodeScanner
+                                        @result="onQrScan"
+                                    />
+                                </v-window-item>
+                            </v-window>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-main>
   </v-app>
 </template>
@@ -33,4 +55,6 @@ function onQrScan (msg: string) {
 const content = ref('')
 
 const debouncedContent = useDebounce(content, 500)
+
+const mode = ref('entry')
 </script>
